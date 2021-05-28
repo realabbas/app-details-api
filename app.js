19642ef319ee707926e1e9d99a47532e2bda599e
realabbas <irizviali@gmail.com>
2021-05-28 13:31:41 +0530

    Add Rapid API Key in Headers for Auth

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const port = 3000;
 const gpsapi = require("./services/scrape-and-map");
 
 app.get("/v1/details/:id", async (req, res) => {
-  console.log(req.params.id);
-  let data = await gpsapi(req.params.id);
-  res.send(data);
+  // const headers = req.headers["x-rapidapi-key"]
+  const headers = req.headers["x-rapidapi-key"];
+  if (headers === "4f39b7dad4msh3b1b5e80f4a0d5fp1c0abajsnf7ddd01e5fa9") {
+    console.log(headers);
+    let data = await gpsapi(req.params.id);
+    res.send(data);
+  } else {
+    res.status(401).json({ message: "Incorrect authorization token" });
+  }
 });
 
 app.listen(port, () => {
