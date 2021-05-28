d772a3fce0c0736c36928ae8b61ff19e82fee8ae
realabbas <irizviali@gmail.com>
2021-05-28 11:35:03 +0530

    ☀️ Add Express Server and Logic

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const gpsapi = require("./services/scrape-and-map");
+
+app.get("/v1/details/:id", async (req, res) => {
+  console.log(req.params.id);
+  let data = await gpsapi(req.params.id);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Google Play Store App Details API running on ${port}`);
+});
